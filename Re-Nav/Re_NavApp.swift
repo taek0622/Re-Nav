cc65acdc155e51c01b01b332c49d4c447c6b0b0a
taek0622 <min4644@gmail.com>
2023-11-16 22:41:51 +0900

    Init: Project setting
    - 프로젝트 기본 세팅

---
+++ Re-Nav/Re_NavApp.swift
@@ -0,0 +1,17 @@
+//
+//  Re_NavApp.swift
+//  Re-Nav
+//
+//  Created by 김민택 on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Re_NavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
