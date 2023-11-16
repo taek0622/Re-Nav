c6873b3b57b516866063b9a2119d1268f2926297
taek0622 <min4644@gmail.com>
2023-11-16 23:24:31 +0900

    Feat:sparkles:: Add TabView
    - 탭 뷰 추가

--- Re-Nav/ContentView.swift
+++ Re-Nav/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            MapView()
+                .tabItem { Label("지도", systemImage: "map") }
+            PinListView()
+                .tabItem { Label("목록", systemImage: "list.bullet") }
+            SettingView()
+                .tabItem { Label("설정", systemImage: "gearshape") }
         }
-        .padding()
     }
 }
 
---
+++ Re-Nav/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  Re-Nav
+//
+//  Created by 김민택 on 11/16/23.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text("SettingView")
+    }
+}
+
+#Preview {
+    SettingView()
+}
