5fc34cf21e2dfe9c28f0e785dec2a2aa48d1360d
taek0622 <min4644@gmail.com>
2023-11-17 22:12:56 +0900

    Design:iphone:: #5 Add NavigationTitle
    - 핀 추가 뷰를 위한 PinCreationView 파일 추가
    - NavigationTitle 추가

---
+++ Re-Nav/PinCreationView.swift
@@ -0,0 +1,24 @@
+//
+//  PinCreationView.swift
+//  Re-Nav
+//
+//  Created by 김민택 on 11/17/23.
+//
+
+import SwiftUI
+
+struct PinCreationView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+            }
+            .navigationTitle("핀 추가")
+            .navigationBarTitleDisplayMode(.inline)
+            .padding(16)
+        }
+    }
+}
+
+#Preview {
+    PinCreationView()
+}
