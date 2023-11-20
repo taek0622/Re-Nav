22915363454ac115482b18781e41ccbedacad530
taek0622 <min4644@gmail.com>
2023-11-20 09:47:10 +0900

    Chore:wrench:: Modify Pin Data Model
    - 지번 주소, 혹은 도로명 주소 중 하나가 없는 경우를 고려하여 데이터 모델 수정

--- Re-Nav/Model/Pin.swift
+++ Re-Nav/Model/Pin.swift
@@ -13,8 +13,8 @@ final class Pin {
     var name: String
     var longitude: Double
     var latitude: Double
-    @Relationship(deleteRule: .cascade) var address: Address
-    @Relationship(deleteRule: .cascade) var roadAddress: RoadAddress
+    @Relationship(deleteRule: .cascade) var address: Address?
+    @Relationship(deleteRule: .cascade) var roadAddress: RoadAddress?
     @Relationship(deleteRule: .nullify) var theme: Theme
     var detail: String
     var photos: [Data]
@@ -22,7 +22,7 @@ final class Pin {
     var createAt: Date
     var updateAt: Date
 
-    init(name: String, longitude: Double, latitude: Double, address: Address, roadAddress: RoadAddress, theme: Theme, detail: String, photos: [Data], rate: Int, createAt: Date, updateAt: Date) {
+    init(name: String, longitude: Double, latitude: Double, address: Address? = nil, roadAddress: RoadAddress? = nil, theme: Theme, detail: String, photos: [Data], rate: Int, createAt: Date, updateAt: Date) {
         self.name = name
         self.longitude = longitude
         self.latitude = latitude
