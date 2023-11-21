8ea159439bf42f5da437ea019d042b2ec9bcd006
taek0622 <min4644@gmail.com>
2023-11-22 01:23:54 +0900

    Feat:sparkles:: #12 Add fetchSearchData
    - searchable 필드에 검색어 입력후 전송시 주소를 검색하는 기능 추가

--- Re-Nav/MapView.swift
+++ Re-Nav/MapView.swift
@@ -14,6 +14,7 @@ struct MapView: View {
     @State private var isPinClicked: Bool = false
     @State private var searchText: String = ""
     @State private var position = GeoCoordinate(longitude: 127.108678, latitude: 37.402001)
+    @State private var searchResult = [SearchDocumentFetchData]()
     let poiTapPub = NotificationCenter.default.publisher(for: NSNotification.Name("PoiTapNotification"))
 
     var body: some View {
@@ -38,6 +39,37 @@ struct MapView: View {
             position = pub.object as! GeoCoordinate
             isPinClicked = true
         })
+        .onSubmit(of: .search) {
+            fetchSearchData(searchText)
+        }
+    }
+
+    private func fetchSearchData(_ searchText: String) {
+        guard var urlComponents = URLComponents(string: "https://dapi.kakao.com/v2/local/search/keyword.json") else { return }
+        urlComponents.queryItems = [
+            URLQueryItem(name: "query", value: searchText)
+        ]
+
+        guard let requestURL = urlComponents.url else { return }
+
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "GET"
+        request.addValue(Bundle.main.object(forInfoDictionaryKey: "KAKAO_REST_API_KEY") as! String, forHTTPHeaderField: "Authorization")
+
+        let defaultSession = URLSession(configuration: .default)
+
+        defaultSession.dataTask(with: request) { data, _, error in
+            if let data  {
+                do {
+                    let decodedData = try JSONDecoder().decode(SearchFetchDataModel.self, from: data)
+                    searchResult = decodedData.documents
+                } catch {
+                    print("Error decoding JSON: \(error.localizedDescription)")
+                }
+            } else if let error {
+                print("Error fetching data: \(error.localizedDescription)")
+            }
+        }.resume()
     }
 }
 
