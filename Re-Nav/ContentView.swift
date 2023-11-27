2b42d83796a6c46035baae885fd35494106c86c0
taek0622 <min4644@gmail.com>
2023-11-28 05:47:45 +0900

    Chore:wrench:: #15 Add showing half modal to tab
    - 탭뷰를 탭해서 하프 모달 보여주는 기능 추가

--- Re-Nav/ContentView.swift
+++ Re-Nav/ContentView.swift
@@ -10,15 +10,81 @@ import SwiftUI
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
 
+    @State private var selectedTab = 1
+    @State private var isShowList = false
+
     var body: some View {
-        TabView {
-            MapView()
-                .tabItem { Label("지도", systemImage: "map") }
-            PinListView()
-                .tabItem { Label("목록", systemImage: "list.bullet") }
-            SettingView()
-                .tabItem { Label("설정", systemImage: "gearshape") }
+        ZStack(alignment: .bottom) {
+            TabView(selection: $selectedTab) {
+                MapView()
+            }
+
+            if selectedTab == 2 {
+                PinListView()
+                    .frame(height: UIScreen.main.bounds.height/10*3)
+                    .transition(.move(edge: .bottom))
+                        .animation(.easeInOut, value: isShowList)
+            }
+
+            if selectedTab == 3 {
+                SettingView()
+            }
+
+            CustomTabView(selectedTab: $selectedTab)
         }
+    }
+}
+
+struct CustomTabView: View {
+    @Binding var selectedTab: Int
+
+    var body: some View {
+        HStack {
+            VStack {
+                Image(systemName: "map.fill")
+                    .resizable()
+                    .frame(width: 23, height: 23)
+                Text("지도")
+                    .font(.system(size: 10))
+            }
+            .frame(width: UIScreen.main.bounds.width/3)
+            .background(.white)
+            .foregroundStyle(selectedTab == 1 ? .blue : .gray)
+            .onTapGesture {
+                selectedTab = 1
+            }
+
+            VStack {
+                Image(systemName: "list.bullet")
+                    .resizable()
+                    .frame(width: 23, height: 23)
+                Text("목록")
+                    .font(.system(size: 10))
+            }
+            .frame(width: UIScreen.main.bounds.width/3)
+            .background(.white)
+            .foregroundStyle(selectedTab == 2 ? .blue : .gray)
+            .onTapGesture {
+                withAnimation {
+                    selectedTab = 2
+                }
+            }
+
+            VStack {
+                Image(systemName: "gearshape.fill")
+                    .resizable()
+                    .frame(width: 23, height: 23)
+                Text("설정")
+                    .font(.system(size: 10))
+            }
+            .frame(width: UIScreen.main.bounds.width/3)
+            .background(.white)
+            .foregroundStyle(selectedTab == 3 ? .blue : .gray)
+            .onTapGesture {
+                selectedTab = 3
+            }
+        }
+        .background(.white)
     }
 }
 
